ede6b5d647d522eba608f1c745c465b0a5b7c5c3
harshit srivastava <srivastavaharshit333@gmail.com>
2020-08-26 14:38:49 +0530

    all files of backend uploaded

---
+++ controllers/profile.js
@@ -0,0 +1,19 @@
+const handleProfileGet = (req,res,db)=>{
+	const {id}=req.params;
+
+	db.select('*').from('users').where({id}).then(users=>{
+        if(users.length)
+        {	
+		res.json(users[0])
+	    }
+	    else{
+
+	    	res.status(400).json('not found')
+	    }
+	}).catch(err=>res.status(400).json('error getting users'));
+
+}
+
+module.exports = {
+	handleProfileGet:handleProfileGet
+}
